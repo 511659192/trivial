5e9f2d580a9f55ac5e5efce113ee520127a29db6
yangmeng44 <cdyangmeng@jd.com>
2016-07-12 20:29:16 +0800

    netty

--- src/test/java/com/ym/netty/EchoClient.java
+++ src/test/java/com/ym/netty/EchoClient.java
@@ -1,94 +1,49 @@
 package com.ym.netty;
 
 import io.netty.bootstrap.Bootstrap;
-
 import io.netty.channel.ChannelFuture;
-
 import io.netty.channel.ChannelInitializer;
-
 import io.netty.channel.ChannelOption;
-
 import io.netty.channel.EventLoopGroup;
-
 import io.netty.channel.nio.NioEventLoopGroup;
-
 import io.netty.channel.socket.SocketChannel;
-
 import io.netty.channel.socket.nio.NioSocketChannel;
-
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-
 import io.netty.handler.codec.LengthFieldPrepender;
 
 public class EchoClient {
-
 	public void connect(String post, int port) throws Exception {
-
 		EventLoopGroup group = new NioEventLoopGroup();
-
 		try {
-
 			Bootstrap b = new Bootstrap();
-
-			b.group(group)
-
-					.channel(NioSocketChannel.class)
-
-					.option(ChannelOption.TCP_NODELAY, true)
-
+			b.group(group).channel(NioSocketChannel.class).option(ChannelOption.TCP_NODELAY, true)
 					.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
-
 					.handler(new ChannelInitializer<SocketChannel>() {
-
 						@Override
-
 						protected void initChannel(SocketChannel arg0) throws Exception {
-
 							arg0.pipeline().addLast("frameDecoder",
 									new LengthFieldBasedFrameDecoder(65535, 0, 2, 0, 2));
-
 							arg0.pipeline().addLast("msgpack decoder", new MsgpackDecoder());
-
 							arg0.pipeline().addLast("frameEncoder", new LengthFieldPrepender(2));
-
 							arg0.pipeline().addLast("msgpack encoder", new MsgpackEncoder());
-
 							arg0.pipeline().addLast(new EchoClientHandler());
-
 						}
-
 					});
-
 			ChannelFuture future = b.connect(post, port).sync();
-
 			future.channel().closeFuture().sync();
-
 		} catch (Exception e) {
-
 			e.printStackTrace();
-
 		} finally {
-
 			group.shutdownGracefully();
-
 		}
-
 	}
 
 	public static void main(String[] args) {
-
 		try {
-
 			new EchoClient().connect("127.0.0.1", 20001);
-
 		} catch (Exception e) {
-
 			// TODO Auto-generated catch block
-
 			e.printStackTrace();
-
 		}
-
 	}
-
 }
--- src/test/java/com/ym/netty/EchoServer.java
+++ src/test/java/com/ym/netty/EchoServer.java
@@ -1,96 +1,50 @@
 package com.ym.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-
 import io.netty.channel.ChannelFuture;
-
 import io.netty.channel.ChannelInitializer;
-
 import io.netty.channel.ChannelOption;
-
 import io.netty.channel.EventLoopGroup;
-
 import io.netty.channel.nio.NioEventLoopGroup;
-
 import io.netty.channel.socket.SocketChannel;
-
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-
 import io.netty.handler.codec.LengthFieldPrepender;
 
 public class EchoServer {
-
 	public void bind(int port) throws Exception {
-
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
-
 		EventLoopGroup workGroup = new NioEventLoopGroup();
-
 		try {
-
 			ServerBootstrap b = new ServerBootstrap();
-
-			b.group(bossGroup, workGroup)
-
-					.channel(NioServerSocketChannel.class)
-
-					.option(ChannelOption.SO_BACKLOG, 102400)
-
+			b.group(bossGroup, workGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 102400)
 					.childHandler(new ChannelInitializer<SocketChannel>() {
-
 						@Override
-
 						protected void initChannel(SocketChannel arg0) throws Exception {
-
 							arg0.pipeline().addLast("frameDecoder",
 									new LengthFieldBasedFrameDecoder(65535, 0, 2, 0, 2));
-
 							arg0.pipeline().addLast("msgpack decoder", new MsgpackDecoder());
-
 							arg0.pipeline().addLast("frameEncoder", new LengthFieldPrepender(2));
-
 							arg0.pipeline().addLast("msgpack encoder", new MsgpackEncoder());
-
 							arg0.pipeline().addLast(new EchoServerHandler());
-
 						}
-
 					});
-
 			ChannelFuture future = b.bind(port).sync();
-
 			future.channel().closeFuture().sync();
-
 		} catch (Exception e) {
-
 			e.printStackTrace();
-
 		} finally {
-
 			bossGroup.shutdownGracefully();
-
 			workGroup.shutdownGracefully();
-
 		}
-
 	}
 
 	public static void main(String[] args) {
-
 		try {
-
 			new EchoServer().bind(20001);
-
 		} catch (Exception e) {
-
 			// TODO Auto-generated catch block
-
 			e.printStackTrace();
-
 		}
-
 	}
-
 }
