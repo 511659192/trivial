738c1bb4646a867b63b8f8b250924efa6f410c40
511659192 <511659192@qq.com>
2017-10-17 19:16:04 +0800

    home

---
+++ src/test/java/com/ym/equal/Home.java
@@ -0,0 +1,11 @@
+package com.ym.equal;
+
+/**
+ * Created by cdyangmeng on 2017/10/17.
+ */
+public class Home {
+
+    public static void main(String[] args) {
+        System.out.println("home");
+    }
+}
