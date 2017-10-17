f43c4a2f66d818d336068997fd72347789f4e601
511659192 <511659192@qq.com>
2017-10-17 19:18:02 +0800

    workerafter merge

---
+++ src/test/java/com/ym/equal/Work.java
@@ -0,0 +1,11 @@
+package com.ym.equal;
+
+/**
+ * Created by cdyangmeng on 2017/10/17.
+ */
+public class Work {
+    public static void main(String[] args) {
+        System.out.println("Before");
+        System.out.println("after");
+    }
+}
